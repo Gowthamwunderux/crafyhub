9b13de6aecb5ac72b0f1115a5a8dce1e1c85d80b
NagarajWunderux <nagarajt@wunderux.com>
2024-01-08 20:59:24 +0530

    push the code

--- src/app/join-us/page.tsx
+++ src/app/join-us/page.tsx
@@ -3,9 +3,15 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function JoinUs() {
+
+
   return (
     <>
-      <div className="w-screen"></div>
+      <div className="w-screen h-screen bg-gradient-to-t from- bg-myGradient to-white flex justify-center items-center">
+        <div
+          className=" border  w-3/4 h-3/4  "
+        ></div>
+      </div>
     </>
   );
 }
