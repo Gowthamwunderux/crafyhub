1cc96ea67b0be65fa0d726e6587a16b63cfc296b
NagarajWunderux <nagarajt@wunderux.com>
2024-01-09 19:16:03 +0530

    join us

--- src/app/assets/index.tsx
+++ src/app/assets/index.tsx
@@ -3,3 +3,5 @@ export { default as freelancers }  from './freelancers.svg'
 export { default as payroll }  from './payroll.svg'
 export {default as CrafyLogo} from './crafy_hub_logo.svg'
 export {default as GLogo} from './G_logo.svg'
+export {default as  freeLancerLogo }from './Freelancer-cuate 1.svg'
+export{ default as clientLogo} from "./Group 16 (1).svg"
--- src/app/join-us/page.tsx
+++ src/app/join-us/page.tsx
@@ -1,16 +1,40 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-
+import { freeLancerLogo, clientLogo } from "../assets";
 export default function JoinUs() {
-
-
+  interface DesignerType {
+    url: string;
+    alt: string;
+    content: string;
+  }
+  const freeLancer: DesignerType = {
+    url: freeLancerLogo,
+    alt: "DesignerLogo",
+    content: "I m a designerlooking for work",
+  };
+  const client: DesignerType = {
+    url: clientLogo,
+    alt: "clientLogo",
+    content: "I m a client, hiring  for project",
+  };
+  const joinUs = [freeLancer, client];
   return (
     <>
-      <div className="w-screen h-screen bg-gradient-to-t from- bg-myGradient to-white flex justify-center items-center">
-        <div
-          className=" border  w-3/4 h-3/4  "
-        ></div>
+      <div className=" w-screen h-screen bg-gradient-to-t from- bg-myGradient to-white flex justify-center items-center ">
+        <div className=" border 2xl:w-3/4 2xl:h-3/4 rounded-2xl xl: w-3/4 xl:h-3/4 sm:w-3/4 sm:h-3/4 md:w-3/4 md:h-3/4">
+          <div className="grid place-items-center 2xl:my-40 2xl:mx-96 sm:grid-cols-1  2xl:grid-cols-2 md:grid-cols-2 ">
+            {joinUs.map((item) => (
+              <>
+                {" "}
+                <div className=" shadow  hover:border-Pink-900  cursor-pointer 2xl: py-8  rounded-2xl">
+                  <Image src={item.url} alt={item.alt}></Image>
+                  <h1 className="font-bold text-center">{item.content}</h1>
+                </div>
+              </>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
